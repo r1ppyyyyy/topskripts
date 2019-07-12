4710cfcb0908922d3c8d3a2bca968cb2f7f65fe6
r1ppyyyyy <52819561+r1ppyyyyy@users.noreply.github.com>
2019-07-12 12:18:08 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,7 @@
+import os
+os.system("cp ts.py $HOME")
+os.system("pkg install python")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("pkg install figlet")
+prunt("Готово")
---
+++ ts.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os
+os.system("clear")
+os.system("figlet TOP-SCRIPTS")
+print ("Spammer-Grab[1]")
+print ("Weemen[2]")
+print ("DarkSploit[3]")
+print ("LazyMux[4]")
+print ("RED-HAWK[5]")
+print ("Fisher[6]")
+print ("7")
+print ("8")
+print ("9")
+print ("Создатель[99]")
+x = input("Введите число:")
+if x =="1":
+        os.system("git clone https://github.com/WattanaGaming/Spammer-Grab-1")
+if x =="2":
+        os.system("git clone https://github.com/evait-security/weeman")
+if x =="3":
+		os.system("git clone https://github.com/LOoLzeC/DarkSploit")
+if x =="4":
+		os.system("git clone https://github.com/Gameye98/Lazymux")
+if x =="5":
+		os.system("https://github.com/Tuhinshubhra/RED_HAWK")
+if x =="6":
+	os.system("https://github.com/foxlitegor/fisher")
+if x =="99":
+	print ("ВК СОЗДАТЕЛЯ https://vk.com/id467309281")
